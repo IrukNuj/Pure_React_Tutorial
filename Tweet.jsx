46c3fbb865af88b26c50b011edeb70d3729e4ca8
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2019-09-03 16:25:38 +0900

    add: like

--- Tweet.jsx
+++ Tweet.jsx
@@ -1,4 +1,7 @@
 function Tweet(props) {
+    const [liked, setLike] = React.useState(false);
+    const toggleLike = React.useCallback(() => setLike((prev) => !prev), [setLike]);
+
     return (
         <div className="tweet">
         <div className="icon-container"><img src={props.icon} alt="" width="50" height="50"/></div>
@@ -8,6 +11,9 @@ function Tweet(props) {
             <span className="account-name">@{props.accountName}</span>
           </div>
           <div className="content">{props.content}</div>
+          <div className="status-action">
+          <span onClick={toggleLike}>{liked ? '❤️' : '♡'}</span>
+        </div>
         </div>
       </div>
     );
